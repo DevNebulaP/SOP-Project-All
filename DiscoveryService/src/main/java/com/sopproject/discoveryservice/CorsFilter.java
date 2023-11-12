0158e60aa637b72388beb1582f38851b5395480e
poompongphun <poompongphun@gmail.com>
2023-11-13 01:03:08 +0700

    update mangeserver

---
+++ DiscoveryService/src/main/java/com/sopproject/discoveryservice/CorsFilter.java
@@ -0,0 +1,25 @@
+package com.sopproject.discoveryservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+public class CorsFilter {
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter();
+    }
+}
